1224c87729982195e03a6e351e9a6d5825d1de01
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-03 14:51:46 -0600

    feat(login): agregar validacion de form

--- src/presentation/views/auth/login/ViewModel.tsx
+++ src/presentation/views/auth/login/ViewModel.tsx
@@ -1,27 +1,62 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
+import { Alert, Platform, ToastAndroid } from 'react-native';
 
 const LoginViewModel = () => {
-    const [values, setValues] = useState({
-        email: '',
-        password: '',
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    if (error !== ''){
+        if (Platform.OS === 'android'){
+            return ToastAndroid.show(error, ToastAndroid.LONG);
+        }
+        Alert.alert('Error', error);
+    }
+  }, [error]);
+
+  const onChange = (value: string, field: string): void => {
+    setValues({
+      ...values,
+      [field]: value,
     });
+  };
 
-    const onChange = (value: string, field: string): void => {
-        setValues({
-            ...values,
-            [field]: value,
-        });
-    };
-
-    const handleLogin = (): void => {
+  const handleLogin = (): void => {
+    if (isValidForm()){
         console.log(values);
-    };
+    }
+  };
 
-    return {
-        ...values,
-        onChange,
-        handleLogin,
-    };
+  const isValidForm = (): boolean => {
+    const reg: RegExp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (values.email === '') {
+      setError('El email no puede estar vacio');
+      return false;
+    }
+    if (values.password === '') {
+      setError('la contraseña no puede estar vacia');
+      return false;
+    }
+    if (values.password.length < 6) {
+      setError('la contraseña debe tener al menos 6 caracteres');
+      return false;
+    }
+    if (reg.test(values.email) === false) {
+      setError('El email no es valido');
+      return false;
+    }
+    return true;
+  };
+
+  return {
+    ...values,
+    onChange,
+    handleLogin,
+  };
 };
 
 export default LoginViewModel;
