fc719c9d2bfd62e7691cdf43d06683db0fae0d62
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-05 13:40:44 -0600

    feat(register): diseñar pantalla

--- src/presentation/views/auth/register/Register.tsx
+++ src/presentation/views/auth/register/Register.tsx
@@ -1,14 +1,78 @@
-import { RootStackParamList } from '../../../navigation/MainStackNavigator';
-import { StackScreenProps } from '@react-navigation/stack';
-import { SafeAreaView, Text } from 'react-native';
+import {DefaultButton} from '../../../components/DefaultButton';
+import {DefaultTextInput} from '../../../components/DefaultTextInput';
+import {Dimensions, Image, Text, TouchableOpacity, View} from 'react-native';
+import {MyColors} from '../../../theme/AppTheme';
+import {RootStackParamList} from '../../../navigation/MainStackNavigator';
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
+import RegisterStyles from './Styles';
+import Svg, {Path} from 'react-native-svg';
 
-interface Props extends StackScreenProps<RootStackParamList, 'RegisterScreen'> {}
+interface Props
+  extends StackScreenProps<RootStackParamList, 'RegisterScreen'> {}
 
 export const RegisterScreen = ({navigation, route}: Props) => {
-    return (
-        <SafeAreaView>
-            <Text>RegisterScreen</Text>
-        </SafeAreaView>
-    );
+  return (
+    <View style={RegisterStyles.container}>
+      <View style={RegisterStyles.svgContainer}>
+        <Svg
+          viewBox="0 0 1440 320"
+          height={500}
+          width={Dimensions.get('window').width}
+          style={RegisterStyles.svg}>
+          <Path
+            fill={MyColors.primary}
+            fillOpacity="1"
+            d="M0 256l48-32c48-32 144-96 240-117.3 96-21.7 192 .3 288 26.6 96 26.7 192 58.7 288 85.4 96 26.3 192 48.3 288 21.3 96-27 192-101 240-138.7l48-37.3V0H0z"
+          />
+        </Svg>
+        <Text style={RegisterStyles.title}>REGISTRATE</Text>
+        <Text style={RegisterStyles.title}>A LA APP</Text>
+        <Image
+          source={require('./../../../../../assets/img/game_con_black.png')}
+          style={RegisterStyles.img}
+        />
+        <TouchableOpacity
+          onPress={() => navigation.pop()}
+          style={RegisterStyles.arrowBackContainer}>
+          <Image
+            source={require('./../../../../../assets/img/left-arrow.png')}
+            style={RegisterStyles.arrowBack}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={RegisterStyles.divider} />
+
+      <DefaultTextInput
+        placeholder="Nombre de usuario"
+        value={''}
+        prop="username"
+        onChangeText={() => {}}
+        image={require('./../../../../../assets/img/user_image.png')}
+      />
+      <DefaultTextInput
+        placeholder="Email"
+        value={''}
+        prop="email"
+        onChangeText={() => {}}
+        image={require('./../../../../../assets/img/email.png')}
+      />
+      <DefaultTextInput
+        placeholder="Contraseña"
+        value={''}
+        prop="password"
+        onChangeText={() => {}}
+        image={require('./../../../../../assets/img/password.png')}
+      />
+      <DefaultTextInput
+        placeholder="Confirmar contraseña"
+        value={''}
+        prop="confirmPassword"
+        onChangeText={() => {}}
+        image={require('./../../../../../assets/img/password.png')}
+      />
+      <DefaultButton text="Registrarme" onPress={() => {}} />
+    </View>
+  );
 };
---
+++ src/presentation/views/auth/register/Styles.tsx
@@ -0,0 +1,43 @@
+import {StyleSheet} from 'react-native';
+import {MyColors} from '../../../theme/AppTheme';
+
+const RegisterStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: MyColors.background,
+  },
+  title: {
+    fontSize: 28,
+    marginLeft: 50,
+    color: MyColors.background,
+  },
+  svgContainer: {
+    backgroundColor: MyColors.primary,
+    height: '30%',
+    paddingTop: 30,
+  },
+  svg: {
+    position: 'absolute',
+    top: -10,
+  },
+  img: {
+    width: 130,
+    height: 130,
+    transform: [{rotate: '35deg'}],
+    alignSelf: 'flex-end',
+  },
+  divider: {
+    flex: 1,
+  },
+  arrowBack: {
+    width: 30,
+    height: 30,
+  },
+  arrowBackContainer: {
+    position: 'absolute',
+    top: 50,
+    left: 10,
+  },
+});
+
+export default RegisterStyles;
