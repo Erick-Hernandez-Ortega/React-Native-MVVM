d81a9473b40a8db2d108efdde585468c52f9be0e
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-07 20:11:29 -0600

    feat(auth): crear datasource

---
+++ src/data/datasource/remote/AuthDataSource.ts
@@ -0,0 +1,19 @@
+// Servicio - Informacion
+import auth from '@react-native-firebase/auth';
+import { Err, Resource, Success } from '../../../domain/utils/Resource';
+
+export interface AuthDataSource {
+    login: (email: string, password: string) => Promise<Resource>;
+}
+
+export const login = async(email: string, password: string): Promise<Resource> => {
+    try {
+        console.log(email, password, 'login');
+        const data = await auth().signInWithEmailAndPassword(email, password);
+        console.log(data, 'data');
+        return Promise.resolve(Success(data));
+    } catch (error) {
+        console.error(error);
+        return Promise.resolve(Err(error));
+    }
+};
