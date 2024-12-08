bef723f2594d85f50d00163bfe0437dbd8153f5c
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-07 20:10:59 -0600

    feat(ioc): agregar dependencias

--- src/di/ioc.ts
+++ src/di/ioc.ts
@@ -1,12 +1,21 @@
-import { asFunction, createContainer } from 'awilix';
+import { asFunction, asValue, createContainer } from 'awilix';
 import LoginViewModel from '../presentation/views/auth/login/ViewModel';
 import RegisterViewModel from '../presentation/views/auth/register/ViewModel';
+import * as AuthDataSource from '../data/datasource/remote/AuthDataSource';
+import { AuthRepository } from '../data/repository/AuthRepository';
+import { LoginUseCase } from '../domain/useCases/auth/LoginUseCase';
 
 const container = createContainer();
 
 container.register({
     LoginViewModel: asFunction(LoginViewModel),
     RegisterViewModel: asFunction(RegisterViewModel),
+
+    authDataSource: asValue(AuthDataSource),
+
+    authRepository: asFunction(AuthRepository),
+
+    LoginUseCase: asFunction(LoginUseCase),
 });
 
 export default container;
