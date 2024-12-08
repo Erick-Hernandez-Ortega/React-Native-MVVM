ac562d845fa1ecac9361ff82b1c2465512df84a0
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-07 19:11:08 -0600

    feat: crear archivo

---
+++ src/domain/utils/Resource.tsx
@@ -0,0 +1,11 @@
+// Estados de la peticion
+// - LOADING
+// - SUCCESS
+// - ERROR
+
+export abstract class Resource {
+    constructor() {}
+}
+export function Loading(): Resource { return {}; }
+export function Success(data: any): Resource { return { data }; }
+export function Err(error: string | any): Resource { return { error}; }
