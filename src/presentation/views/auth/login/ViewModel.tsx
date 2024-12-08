8946b161a8612fad37a93fa02c8713fe8d178077
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-07 20:12:05 -0600

    feat(auth): agregar funcionalidad de login

--- src/presentation/views/auth/login/ViewModel.tsx
+++ src/presentation/views/auth/login/ViewModel.tsx
@@ -1,7 +1,8 @@
 import {useEffect, useState} from 'react';
 import { Alert, Platform, ToastAndroid } from 'react-native';
+import { LoginUseCaseInterface } from '../../../../domain/useCases/auth/LoginUseCase';
 
-const LoginViewModel = () => {
+const LoginViewModel = ({ LoginUseCase }: { LoginUseCase: LoginUseCaseInterface }) => {
   const [values, setValues] = useState({
     email: '',
     password: '',
@@ -24,28 +25,30 @@ const LoginViewModel = () => {
     });
   };
 
-  const handleLogin = (): void => {
+  const handleLogin = async(): Promise<void> => {
     if (isValidForm()){
         console.log(values);
+        const data = await LoginUseCase.execute(values.email.trim(), values.password.trim());
+        console.log(data, 'data');
     }
   };
 
   const isValidForm = (): boolean => {
     const reg: RegExp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (values.email === '') {
+    if (values.email.trim() === '') {
       setError('El email no puede estar vacio');
       return false;
     }
-    if (values.password === '') {
+    if (values.password.trim() === '') {
       setError('la contraseña no puede estar vacia');
       return false;
     }
-    if (values.password.length < 6) {
+    if (values.password.trim().length < 6) {
       setError('la contraseña debe tener al menos 6 caracteres');
       return false;
     }
-    if (reg.test(values.email) === false) {
+    if (reg.test(values.email.trim()) === false) {
       setError('El email no es valido');
       return false;
     }
