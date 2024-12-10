b9c29242f653b9e3b83ed9a9384b0f336063b700
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-10 15:28:29 -0600

    feat: crear metodo de registro

--- src/data/datasource/remote/AuthDataSource.ts
+++ src/data/datasource/remote/AuthDataSource.ts
@@ -1,14 +1,25 @@
 // Servicio - Informacion
 import auth from '@react-native-firebase/auth';
+import { User } from '../../../domain/models/User';
 
 export interface AuthDataSource {
     login: (email: string, password: string) => Promise<any>;
+    register: (user: User) => Promise<any>;
 }
 
 export const login = async(email: string, password: string): Promise<any> => {
     try {
-        console.log(email, password, 'login');
         const data = await auth().signInWithEmailAndPassword(email, password);
+        return Promise.resolve({ error: null, result: data });
+    } catch (error: any) {
+        console.error(error);
+        return Promise.resolve({ error: error.message, result: null });
+    }
+};
+
+export const register = async(user: User): Promise<any> => {
+    try {
+        const data = await auth().createUserWithEmailAndPassword(user.email, user.password);
         return Promise.resolve({ error: null, result: data });
     } catch (error: any) {
         console.error(error);
--- src/data/repository/AuthRepository.ts
+++ src/data/repository/AuthRepository.ts
@@ -1,13 +1,19 @@
+import { User } from '../../domain/models/User';
 import { AuthDataSource } from '../datasource/remote/AuthDatasource';
 
 export interface AuthRepository {
     login: (email: string, password: string) => Promise<any>;
+    register: (user: User) => Promise<any>;
 }
 
 export const AuthRepository = ({ authDataSource }: { authDataSource: AuthDataSource }) => {
     return {
         async login (email: string, password: string) {
             const { result, error } = await authDataSource.login(email, password);
+            return { result, error };
+        },
+        async register (user: User) {
+            const { result, error } = await authDataSource.register(user);
             return { result, error };
         },
     };
