4d4477896088cc869708a76b157b53de80287ac9
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-10 13:37:44 -0600

    feat: agregar metodo Init

--- src/domain/utils/Resource.tsx
+++ src/domain/utils/Resource.tsx
@@ -9,3 +9,4 @@ export abstract class Resource {
 export function Loading(): Resource { return {}; }
 export function Success(data: any): Resource { return { data }; }
 export function Err(error: string | any): Resource { return { error}; }
+export function Init(): Resource { return {}; }
