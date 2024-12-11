e4e44ab1c5e37cb8798848cdda9120510413b3c3
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-11 13:39:24 -0600

    feat(home): hacer logout

--- src/presentation/views/home/Home.tsx
+++ src/presentation/views/home/Home.tsx
@@ -1,16 +1,25 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../navigation/MainStackNavigator';
 import { Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import HomeStyles from './Styles';
+import DI from '../../../di/ioc';
 import { DefaultButton } from '../../components/DefaultButton';
 
 interface Props extends StackScreenProps<RootStackParamList, 'HomeScreen'> {}
 
-export const HomeScreen = ({navigation, route}: Props) => {
+export const HomeScreen = ({navigation}: Props) => {
+    const { _result, logout } = DI.resolve('HomeViewModel');
+
+    useEffect(() => {
+      if (_result) {
+        navigation.replace('LoginScreen');
+      }
+    }, [_result, navigation]);
+
     return (
         <View style={HomeStyles.container}>
-            <DefaultButton text="Cerrar Sesion" onPress={() => {}}/>
+            <DefaultButton text="Cerrar Sesion" onPress={logout}/>
             <Text>Home</Text>
         </View>
     );
