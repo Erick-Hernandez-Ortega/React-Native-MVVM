daa7614b28caaa4ef38e4c8d25c66dec280717c8
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-12 14:22:08 -0600

    feat: modificar use case

--- src/domain/useCases/auth/GetUserUseCase.ts
+++ src/domain/useCases/auth/GetUserUseCase.ts
@@ -2,8 +2,8 @@ import { AuthRepository } from '../../../data/repository/AuthRepository';
 
 export const GetUserUseCase = ({ authRepository }: { authRepository: AuthRepository }) => {
     return {
-        async run() {
-            const { result, error } = await authRepository.getUser();
+        run() {
+            const { result, error } = authRepository.getUser();
             return { result, error };
         },
     };
