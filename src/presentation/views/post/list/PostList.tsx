1cb816e1250244e8247b7f9bfb420e1d1866e820
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-12-13 19:16:07 -0600

    fix: comentar di

--- src/presentation/views/post/list/PostList.tsx
+++ src/presentation/views/post/list/PostList.tsx
@@ -2,13 +2,12 @@ import { StackScreenProps } from '@react-navigation/stack';
 import { Text, View } from 'react-native';
 import React from 'react';
 import PostListStyles from './Styles';
-import DI from '../../../../di/ioc';
+// import DI from '../../../../di/ioc';
 import { TabParamList } from '../../../navigation/Tabsnavigator';
 
 interface Props extends StackScreenProps<TabParamList, 'PostListScreen'> {}
 
 export const PostListScreen = ({navigation}: Props) => {
-    const { } = DI.resolve('');
     return (
         <View style={PostListStyles.container}>
             <Text>PostList</Text>
--- src/presentation/views/post/myList/MyPostList.tsx
+++ src/presentation/views/post/myList/MyPostList.tsx
@@ -2,13 +2,11 @@ import { StackScreenProps } from '@react-navigation/stack';
 import { Text, View } from 'react-native';
 import React from 'react';
 import MyPostListStyles from './Styles';
-import DI from '../../../../di/ioc';
 import { TabParamList } from '../../../navigation/Tabsnavigator';
 
 interface Props extends StackScreenProps<TabParamList, 'MyPostListScreen'> {}
 
 export const MyPostListScreen = ({navigation}: Props) => {
-    const { } = DI.resolve('');
     return (
         <View style={MyPostListStyles.container}>
             <Text>MyPostListScreen</Text>
